97929b886c8dad03d47921ab9acabf4b1c942b0b
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-10-23 09:47:19 -0700

    10.23.18

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,1 +1,29 @@
 print ("Hello World!")
+
+# This is a comment. This has no effect on the code
+# but this does not allow me to do things. I can:
+# 1. Make notes to myself
+# 2. Comment pieces of code that do not work
+# 3. Make my code easier to read
+
+print ("Look at what happened here. Is there any space?")
+print()
+print()
+print("There should be a couple black lines here.")
+
+#Math
+print(3 + 5)
+print(5 - 2)
+print(3 * 4)
+print(6 / 2)
+
+print("Figure this out.")
+print(6 // 2)
+print(5 // 2)
+print(9 // 4)
+
+print("Here is another one...")
+print(6 % 2)
+print(5 % 2)
+print(11 % 4)  #Modulus (Remainder)
+
