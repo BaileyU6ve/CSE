55efbbf30f70a7bd36815370767a5c0e9f6f3c04
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-11-01 09:45:33 -0700

    11.1.18

---
+++ notes/Bailey Phaphone - Guess Game.py
@@ -0,0 +1,15 @@
+import random
+
+print("Guess a number from 1 to 10")
+Guess1 = input("Guess-")
+
+Guess2 = input("Guess-")
+
+Guess3 = input("Guess-")
+
+Guess4 = input("Guess-")
+
+Guess5 = input("Guess-")
+
+
+print(random.randint(1, 10))
