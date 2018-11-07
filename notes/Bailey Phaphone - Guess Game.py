b119f7bd5b51cb35785bb18133dd4ca9119e8343
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-11-07 09:43:23 -0800

    11.7.18

--- notes/Bailey Phaphone - Guess Game.py
+++ notes/Bailey Phaphone - Guess Game.py
@@ -1,9 +1,16 @@
 import random
-r = random.randint(1, 10)
-print("Guess a number from 1 to 10")
-
-while r and guess:
-    
-
+number = random.randint(1, 10)
+guesses = 5
+while guesses > 0:
+    num = int(input("What's a number from 1 to 10-"))
+    if num > number:
+        print("Lower!")
+        guesses = guesses - 1
+    elif num < number:
+        print("Higher!")
+        guesses = guesses - 1
+    elif num == number:
+        print("Correct!")
+        guesses = 0
 
 
