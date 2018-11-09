c4ca9cd0f58c3f4444fe36cbbd47c7990894d265
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-11-09 10:36:53 -0800

    11.9.18

--- notes/Lucky 7's.py
+++ notes/Lucky 7's.py
@@ -5,17 +5,38 @@ money = 15
 rounds = 0
 while money > 0:
     if dice1 + dice2 == 7:
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print()
+        print("You rolled")
+        print(dice1 + dice2)
         money = money + 5
+        print("Current cash")
+        print(money)
+        print()
         rounds = rounds + 1
-        print(money)
     elif dice1 + dice2 < 7:
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print()
+        print("You rolled")
+        print(dice1 + dice2)
         money = money - 1
+        print("Current cash")
         print(money)
+        print()
         rounds = rounds + 1
     elif dice1 + dice2 > 7:
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print("You rolled")
+        print(dice1 + dice2)
         money = money - 1
+        print("Current cash")
         print(money)
+        print()
         rounds = rounds + 1
+print()
 print("You're Broke")
 print("Rounds Played -")
 print(rounds)
