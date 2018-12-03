cec01d7f14ab780cd78a141671c4cd5d5dab9aba
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-12-03 09:45:47 -0800

    12.3.18

---
+++ notes/Bailey Phaphone - Challenges.py
@@ -0,0 +1,11 @@
+"""
+# Challenge 1
+def challenge1(firstname, lastname):
+    return lastname, firstname
+
+
+print(challenge1("Bailey", "Phaphone"))
+"""
+
+# Challenge 2
+
--- notes/hello world.py
+++ notes/hello world.py
@@ -246,3 +246,12 @@ for charcters in list1:
 '''
 # Turn a list into a string
 print("".join(list1))
+
+
+# Function Notes
+# a**2 + b**2 = c**2
+def pythagorean(a, b):
+    return (a**2 + b**2)**(1/2)
+
+
+print(pythagorean(3, 4))
