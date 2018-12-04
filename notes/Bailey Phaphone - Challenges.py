a273b383e0cac95da0988248862d3bb4ace64b71
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-12-04 09:45:08 -0800

    12.4.18

--- notes/Bailey Phaphone - Challenges.py
+++ notes/Bailey Phaphone - Challenges.py
@@ -5,7 +5,22 @@ def challenge1(firstname, lastname):
 
 
 print(challenge1("Bailey", "Phaphone"))
+
+
+# Challenge 3
+def triangle_formula(b, h):
+    return(b * h)**(1/2)
+
+print(triangle_formula(3,8))
 """
 
-# Challenge 2
+# Challenge 4
+number = 3
+if number == 0:
+    print("The number is neither positive or negative")
+else number < 0:
+    print("The number is a positive")
+else number > 0:
+print
+
 
