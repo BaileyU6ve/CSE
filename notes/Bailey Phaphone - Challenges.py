f0e66adbbe7c87fac112692c21fe9d6282044896
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2018-12-05 09:47:42 -0800

    12.5.18

--- notes/Bailey Phaphone - Challenges.py
+++ notes/Bailey Phaphone - Challenges.py
@@ -1,26 +1,59 @@
-"""
+
 # Challenge 1
+
+
 def challenge1(firstname, lastname):
     return lastname, firstname
 
 
 print(challenge1("Bailey", "Phaphone"))
 
+print()
 
 # Challenge 3
+
+
 def triangle_formula(b, h):
     return(b * h)**(1/2)
 
+
+print("Area of triangle=")
 print(triangle_formula(3,8))
-"""
+
+print()
 
 # Challenge 4
-number = 3
-if number == 0:
-    print("The number is neither positive or negative")
-else number < 0:
-    print("The number is a positive")
-else number > 0:
-print
+
+
+def number(integer):
+    if integer > 0:
+        return "The number is a positive"
+    elif integer < 0:
+        return "The number is a negative"
+    elif integer == 0:
+        return "The number is neither positive or negative"
+
+
+The_number = number(3)
+print(The_number)
+
+print()
+
+# Challenge 5
+
+
+def circle(r):
+    return(r**2)*3.14
+
+
+print("Area of circle=")
+print(circle(4))
+
+# Challenge 6
+
+
+def sphere(f):
+    return(f**3)*4/3*3.14
+
 
 
