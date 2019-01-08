cb6b5f0ff5b7a2e1825a83052503ae0b9d0467e7
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-01-08 09:44:57 -0800

    1.8.19
    semester 2 notes

---
+++ Semester 2 Notes.py
@@ -0,0 +1,20 @@
+print("hello world")
+
+# Comment
+
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+print("I have " + str(cars) + " cars")
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age.")
+
+colors = ["green", "blue", "orange", "yellow", "red"]
+colors.append("purple")
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[2])
+print(len(colors))
