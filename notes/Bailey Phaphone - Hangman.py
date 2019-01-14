1f58d1eade5b2509b338c948ffcb199169939b5b
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-01-14 09:45:00 -0800

    1.14.19

--- notes/Bailey Phaphone - Hangman.py
+++ notes/Bailey Phaphone - Hangman.py
@@ -1,7 +1,9 @@
 import random
-guesses = 8
+guesses = 6
 word_bank = ["phoenix", "amazing", "ice cream", "sync", "incantation", "pandemonium", "isosceles", "California",
              "xylophone", "Yggdrasil"]
 
 word = random.choice(word_bank)
+list1 = list(word)
 print(word)
+
