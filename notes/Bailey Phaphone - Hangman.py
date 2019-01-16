e6b218585a93b69bc11f9c8838d51351f14ae745
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-01-16 09:44:54 -0800

    1.16.19

--- notes/Bailey Phaphone - Hangman.py
+++ notes/Bailey Phaphone - Hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 word_bank = ["phoenix", "amazing", "ice cream", "sync", "incantation", "pandemonium", "isosceles", "California",
              "xylophone", "Yggdrasil"]
 
@@ -8,5 +9,19 @@ LettersCorrect = []
 LettersIncorrect = []
 Letters = (len(word))
 
+string1 = word
+list1 = list(string1)
+print(list1)
 
-while guesses > 0 and not 
+alphabet = string.ascii_letters
+
+for i in range(len(list1)):
+        if list1[i] == alphabet:
+            list1.pop(i)
+            list1.insert(i, "*")
+
+
+print("".join(list1))
+
+
+
