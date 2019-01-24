170d18b563b838c589392bb65f65e5e96ac778e6
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-01-24 09:44:36 -0800

    1.24.19

--- notes/Bailey Phaphone - Hangman.py
+++ notes/Bailey Phaphone - Hangman.py
@@ -7,11 +7,14 @@ guesses = 8
 word = random.choice(word_bank)
 list1 = list(word)
 letters_guessed = []
+output = []
 letters = (len(word))
 alphabet = string.ascii_letters
+
+for i in range(letters):
+    output.append("*")
+print("".join(output))
+print(word)
+
 guess = input("Guess a letter-")
-
 letters_guessed.append(guess)
-
-for i in range(list1):
-  
