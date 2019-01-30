bd3ab0325f4a30006d890bf45cf76495879ca3e4
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-01-30 09:44:48 -0800

    1.30.19

--- notes/Dictionary Notes.py
+++ notes/Dictionary Notes.py
@@ -29,19 +29,27 @@ nested_dictionary = {
 
 print(nested_dictionary["GA"]["POPULATION"])
 print(nested_dictionary["FL"]["NAME"])
-print(nested_dictionary["GA"]
+print(nested_dictionary["GA"])
 
 
 complex_dictionary = {
     "CA": {
         "NAME": "California",
-        "POPULATION": 39500000
+        "POPULATION": 39500000,
+        "CITIES": [
+            "Fresno",
+            "San Francisco",
+            "Los Angeles"
+        ]
     },
     "FL": {
         "NAME": "Florida",
         "POPULATION": 21300000,
         "CITIES": [
-            ""
+            "Miami",
+            "Orlando",
+            "Tampa",
+            "Jacksonville"
         ]
     },
     "AK": {
@@ -64,3 +72,25 @@ complex_dictionary = {
     }
 }
 print(complex_dictionary["AK"]["CITIES"][0])
+
+print(complex_dictionary["FL"]["NAME"])
+print(complex_dictionary["GA"]["CITIES"][0])
+
+print(complex_dictionary.keys())
+print(complex_dictionary.items())
+print(nested_dictionary.items())
+
+for key, value in complex_dictionary.items():
+    print(key)
+    print(value)
+    print("-" * 20)
+
+
+# This is what makes it looks pretty
+print()
+for state, info in complex_dictionary.items():
+    for label, stats in info. items():
+        print(label)
+        print(stats)
+        print("-" * 20)
+    print("=" * 20)
