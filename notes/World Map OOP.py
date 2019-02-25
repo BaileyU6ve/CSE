043430cbc3eb58fae0194c19988e51cc0abe508b
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-02-25 09:43:10 -0800

    2.25.19

--- notes/World Map OOP.py
+++ notes/World Map OOP.py
@@ -1,11 +1,10 @@
 class Room(object):
-    def __init__(self, name, north=None, south=None, east=None, west=None, up=None, down=None):
+    def __init__(self, name, north=None, south=None, east=None, west=None):
         self.name = name
         self.north = north
         self.south = south
         self.east = east
         self.west = west
-       
 
 
 # Option 1 - Define as we go
@@ -23,8 +22,8 @@ entrance = Room('Entrance', 'carousel', None, None, None)
 carousel = Room('Carousel', None, 'entrance', 'restrooms', 'maze')
 restrooms = Room('Restrooms', None, None, None, 'carousel')
 maze = Room('Bush Maze', 'adventure_land', 'dead_end', 'carousel', None)
-dead_end = Room('DEAD END', 'maze')
-adventure_land = Room('Adventure Land', 'bumper_cars', 'maze', 'rocket_coaster')
+dead_end = Room('DEAD END', 'maze', None, None, None)
+adventure_land = Room('Adventure Land', 'bumper_cars', 'maze', 'rocket_coaster', None)
 bumper_cars = Room('Bumper cars', None, adventure_land, None, None)
 rocket_coaster = Room('Rocket roller coaster,', None, None, 'train_station', 'adventure_land')
 train_station = Room('Train Station', 'split_path', None, None, 'rocket_coaster')
