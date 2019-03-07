327a5b5b01f472a6a72a6435a52ad0bce425ddc1
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-03-07 09:44:15 -0800

    3.7.19

--- notes/Item.py
+++ notes/Item.py
@@ -21,20 +21,54 @@ class Knife(Weapon):
 
 class Pistol(Weapon):
     def __init__(self):
-        super(Pistol, self).__init__("Pistol", 5, 100)
+        super(Pistol, self).__init__("Pistol", 5, 80)
+
+    def shoot(self):
+        self.durability -= 1
+        print("You're ammo is getting lower.")
 
 
 class Machete(Weapon):
     def __init__(self):
         super(Machete, self).__init__("Machete", 40, 75)
 
+
 class Armor(Item):
-    def __init__(self, name, protection, durability):
+    def __init__(self, name, protection):
         super(Armor, self).__init__(name)
         self.protection = protection
-        
+        self.equip = False
+
+    def equip(self):
+        self.equip = True
+
+    def unequip(self):
+        self.equip = False
 
 
+class Vest1(Armor):
+    def __init__(self):
+        super(Vest1, self).__init__("Armor Vest level 1", 5)
 
+
+class Vest2(Armor):
+    def __init__(self):
+        super(Vest2, self).__init__("Armor Vest level 2", 15)
+
+
+class Vest3(Armor):
+    def __init__(self):
+        super(Vest3, self).__init__("Armor Vest level 3", 30)
+
+
+class Consumable(Item):
+    def __init__(self, name, recovery):
+        super(Consumable, self).__init__(name)
+        self.recovery = recovery
+
+
+class Apple(Consumable):
+    def __init__(self):
+        super(Apple, self).__init__("Apple", 5)
 
 
