ca14b5d8a38ec65cd6ec2c4674a1cde18d400783
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-03-08 09:44:45 -0800

    3.8.19

--- notes/Item.py
+++ notes/Item.py
@@ -9,28 +9,37 @@ class Weapon(Item):
         self.damage = damage
         self.durability = durability
 
-    def use_weapon(self):
-        self.durability -= 5
+    def swing_weapon(self):
+        self.durability -= 1
         print("Your weapon's durability gets lower.")
 
 
 class Knife(Weapon):
     def __init__(self):
-        super(Knife, self).__init__("Pocket Knife", 20, 55)
+        super(Knife, self).__init__("Pocket Knife", 15, 10)
 
 
 class Pistol(Weapon):
     def __init__(self):
-        super(Pistol, self).__init__("Pistol", 5, 80)
+        super(Pistol, self).__init__("Pistol", 5, 50)
 
-    def shoot(self):
+    def shootpistol(self):
         self.durability -= 1
         print("You're ammo is getting lower.")
 
 
 class Machete(Weapon):
     def __init__(self):
-        super(Machete, self).__init__("Machete", 40, 75)
+        super(Machete, self).__init__("Machete", 25, 20)
+
+
+class Revolver(Weapon):
+    def __init__(self):
+        super(Revolver, self).__init__("Revolver", 50, 5)
+
+    def shootrevolver(self):
+        self.durability -= 5
+        print("The revolver barely has any ammo.")
 
 
 class Armor(Item):
@@ -39,26 +48,35 @@ class Armor(Item):
         self.protection = protection
         self.equip = False
 
-    def equip(self):
-        self.equip = True
-
-    def unequip(self):
-        self.equip = False
-
 
 class Vest1(Armor):
     def __init__(self):
-        super(Vest1, self).__init__("Armor Vest level 1", 5)
+        super(Vest1, self).__init__("level 1 Armor Vest ", 5)
 
 
 class Vest2(Armor):
     def __init__(self):
-        super(Vest2, self).__init__("Armor Vest level 2", 15)
+        super(Vest2, self).__init__("level 2 Armor Vest ", 15)
 
 
 class Vest3(Armor):
     def __init__(self):
-        super(Vest3, self).__init__("Armor Vest level 3", 30)
+        super(Vest3, self).__init__("level 3 Armor Vest ", 30)
+
+
+class Gloves1(Armor):
+    def __init__(self):
+        super(Gloves1, self).__init__("Level 1 Gloves ", 2)
+
+
+class Gloves2(Armor):
+    def __init__(self):
+        super(Gloves2).__init__("Level 2 Gloves", 5)
+
+
+class Gloves3(Armor):
+    def __init__(self):
+        super(Gloves3).__init__("Level 3 Gloves", 10)
 
 
 class Consumable(Item):
@@ -72,3 +90,20 @@ class Apple(Consumable):
         super(Apple, self).__init__("Apple", 5)
 
 
+class Bread(Consumable):
+    def __init__(self):
+        super(Bread, self).__init__("Bread", 10)
+
+
+class Hamburger(Consumable):
+    def __init__(self):
+        super(Hamburger, self).__init__("Hamburger", 50)
+
+
+class Tape(Consumable):
+    def __init__(self):
+        super(Tape, self).__init__("Tape", 0)
+
+    def reload(self):
+        self.durability += 5
+        print("Your weapons have been repaired.")
