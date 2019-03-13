fe7688f50753ebb28df321b29cb3800e1402eb2c
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-03-13 09:46:18 -0700

    3.13.19

--- notes/Item.py
+++ notes/Item.py
@@ -111,6 +111,36 @@ class Tape(Consumable):
     def __init__(self):
         super(Tape, self).__init__("Tape", 0)
 
-    def repair(self):
-        self.durability += 5
-        print("Your weapons have been repaired.")
+
+class Character(object):
+    def __init__(self, name, health: int, weapon, armor):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+        self.armor = armor
+
+    def take_damage(self, damage: int):
+        if self.armor.armor_amt > damage:
+            print("No damage is done because of some AMAZING armor.")
+        else:
+            self.health -= damage - self.armor.armor_amt
+            print("%s has %d health left" % (self.name, self.health))
+
+    def attack(self, target):
+        print("%s attacks %s for %d damage" % (self.name, target.name, self.weapon. damage))
+        target.take_damage(self.weapon.damage)
+
+
+# Items
+sword = Weapon("Sword", 10, 10)
+canoe = Weapon("Canoe", 42, 10)
+wiebe_armor = Armor("Armor of the gods", 10000000000000000000000000000000000)
+
+# Characters
+orc = Character("Orc1", 100, sword, Armor("Generic Armor ", 2))
+orc2 = Character("Wiebe", 10000, canoe, wiebe_armor)
+
+orc.attack(orc2)
+orc2.attack(orc)
+orc2.attack(orc)
+orc2.attack(orc)
