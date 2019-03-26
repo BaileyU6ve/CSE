f8851ac35143216f7255969878c4512c3fb567e0
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-03-26 09:44:20 -0700

    3.26.19

--- notes/Whatever.py
+++ notes/Whatever.py
@@ -83,12 +83,12 @@ class Gloves1(Armor):
 
 class Gloves2(Armor):
     def __init__(self):
-        super(Gloves2).__init__("Level 2 Gloves", 5)
+        super(Gloves2, self).__init__("Level 2 Gloves", 5)
 
 
 class Gloves3(Armor):
     def __init__(self):
-        super(Gloves3).__init__("Level 3 Gloves", 10)
+        super(Gloves3, self).__init__("Level 3 Gloves", 10)
 
 
 class Consumable(Item):
@@ -185,51 +185,51 @@ class Player(object):
 
 
 entrance = Room('Entrance', 'carousel', None, None, None, "You're right outside of the amusement "
-                                                          "park. Everything's dark and abandoned.", Apple, None)
+                                                          "park. Everything's dark and abandoned.", Apple(), None)
 carousel = Room('Carousel', None, 'entrance', 'restrooms', 'maze', "The rides should be out of order. You can see the "
                                                                    "restrooms to the east. There's a bush maze "
-                                                                   "opposite of it.", Knife, None)
+                                                                   "opposite of it.", Knife(), None)
 restrooms = Room('Restrooms', None, None, None, 'carousel', "There's a row of stalls in each "
-                                                            "restroom. Nothing works anymore.", Gloves1, None)
+                                                            "restroom. Nothing works anymore.", Gloves1(), None)
 maze = Room('Bush Maze', 'adventure_land', 'dead_end', 'carousel', None, "There's only one exit in the maze. "
                                                                          "You feel a murderous presence "
-                                                                         "towards the south.", Pistol, None)
+                                                                         "towards the south.", Pistol(), None)
 dead_end = Room('DEAD END', 'maze', None, None, None, "You find clowns waiting at the end."
-                                                      "You should turn back around.", Bread, None)
+                                                      "You should turn back around.", Bread(), None)
 adventure_land = Room('Adventure Land', 'bumper_cars', 'maze', 'rocket_coaster', None, "This is where most of "
                                                                                        "the rides were. "
                                                                                        "There's a ride straight ahead "
                                                                                        "and another to the right.",
-                      Vest1, None)
+                      Vest1(), None)
 bumper_cars = Room('Bumper cars', None, 'adventure_land', None, None, "There's someone riding one of the bumper cars. "
                                                                       "The rides shouldn't be working anymore.",
-                   Gloves2, None)
+                   Gloves2(), None)
 rocket_coaster = Room('Rocket roller coaster,', None, None, 'train_station', 'adventure_land', "Half of the roller "
                                                                                                "coaster is hanging "
                                                                                                "off of the rails."
                                                                                                "Many accidents "
                                                                                                "happened here.",
-                      Revolver, None)
+                      Revolver(), None)
 train_station = Room('Train Station', 'split_path', None, None, 'rocket_coaster', "You can still ride the train "
-                                                                                  "north form here.", Vest2, None)
+                                                                                  "north form here.", Vest2(), None)
 split_path = Room('Split path', None, 'train_station', 'hole', 'tiny_town', "You can go east or west from here. "
                                                                             "You can hear music coming from the west.",
-                  Bread, None)
+                  Bread(), None)
 hole = Room('Broken Path', None, None, None, 'split_path', "The rails are broken here and they lead to a "
-                                                           "hole. Anymore and you would've fallen in", Hamburger, None)
+                                                           "hole. Anymore and you would've fallen in", Hamburger(), None)
 tiny_town = Room('Tiny Town', 'fountain', None, 'split_path', None, "This place is all machine, "
-                                                                    "including the people.", Machete, None)
+                                                                    "including the people.", Machete(), None)
 fountain = Room('Fountain', 'food_area', 'tiny_town', 'park', None, "No water flowed from this fountain anymore. "
                                                                     "Looks like it was the center of the town",
-                Vest3, None)
+                Vest3(), None)
 park = Room('Tiny Park', None, None, None, 'fountain', "Nothing in the park is green and "
-                                                       "most of the benches were broken.", Gloves3, None)
+                                                       "most of the benches were broken.", Gloves3(), None)
 food_area = Room('Food Alley', None, 'fountain', None, 'haunted_house', "All the food used to be sold here. "
                                                                         "You can hear a child crying from the west.",
-                 Tape, None)
+                 Tape(), None)
 haunted_house = Room('Haunted Mansion', None, None, 'food_area', None, "This is the farthest you can get in the park. "
                                                                        "The child's cries are coming from here.",
-                     Hamburger, None)
+                     Hamburger(), None)
 
 
 # Weapons
@@ -275,7 +275,7 @@ directions = ['north', 'south', 'east', 'west', 'up', 'down']
 while playing:
     print(player.current_location.name)
     print(player.current_location.description)
-    print("There's a %s in the area." % Item)
+    print("There's a %s around." % player.current_location.item.name)
     command = input(">_")
     if command.lower() in ['q', 'quit', 'exit']:
         playing = False
@@ -289,8 +289,8 @@ while playing:
         print("Command Not Found")
 
 """
-1. Put Items in room (◉ω◉)
-2. Show item is in the room
+1. Put Items in room  (◉ω◉)
+2. Show item is in the room  (◉ω◉)
 3. Pick up the item
 4. Use the item
 """
