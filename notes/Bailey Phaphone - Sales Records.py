0bbcdfdb826e898b353c3938cbbb865394794331
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-05-08 09:46:59 -0700

    5.8.19

--- notes/Bailey Phaphone - Sales Records.py
+++ notes/Bailey Phaphone - Sales Records.py
@@ -1,3 +1,16 @@
 import csv
 
-print()
+with open("Sales Records.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            row = (row[13])
+
+
+def biggest_num(list):
+    tp = list(row[13])
+    print("The biggest number is: %s" % max(tp))
+
+
+print(biggest_num)
