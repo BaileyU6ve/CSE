305dc418b2041917517e94cf81ebeb53a658a27e
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-05-10 10:38:53 -0700

    5.10.19

--- notes/Bailey Phaphone - Sales Records.py
+++ notes/Bailey Phaphone - Sales Records.py
@@ -5,13 +5,8 @@ with open("Sales Records.csv", 'r') as old_csv:
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
         for row in reader:
-            row = (row[2])
-
-
-def total():
-    sum(row[13]) if row[2] == "Fruits" else None
-
-
-
-
+            if row[2] == "Fruits":
+                print(row[2])
+                print(sum(row[13]))
+                print()
 
