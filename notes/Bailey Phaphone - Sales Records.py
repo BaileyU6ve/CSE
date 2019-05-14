619929bfeabf16803a313d3033b55f862a04d874
BaileyU6ve <42385626+BaileyU6ve@users.noreply.github.com>
2019-05-14 09:45:56 -0700

    5.14.19

--- notes/Bailey Phaphone - Sales Records.py
+++ notes/Bailey Phaphone - Sales Records.py
@@ -5,13 +5,70 @@ with open("Sales Records.csv", 'r') as old_csv:
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
         item_list = []
-        total = 0
+        totalA = 0
+        totalB = 0
+        totalC = 0
+        totalD = 0
+        totalE = 0
+        totalF = 0
+        totalG = 0
+        totalH = 0
+        totalI = 0
+        totalJ = 0
+        totalK = 0
+        totalL = 0
         for row in reader:
             if row[0] == "Region":
                 continue
             item_type = row[2]
+            if item_type not in item_list:
+                item_list.append(item_type)
             if row[2] == "Fruits":
-                total += float(row[13])
-        print(total)
+                totalA += float(row[13])
+            if row[2] == "Clothes":
+                totalB += float(row[13])
+            if row[2] == "Meat":
+                totalC += float(row[13])
+            if row[2] == "Beverages":
+                totalD += float(row[13])
+            if row[2] == "Office Supplies":
+                totalE += float(row[13])
+            if row[2] == "Cosmetics":
+                totalF += float(row[13])
+            if row[2] == "Snacks":
+                totalG += float(row[13])
+            if row[2] == "Personal Care":
+                totalH += float(row[13])
+            if row[2] == "Household":
+                totalI += float(row[13])
+            if row[2] == "Vegetables":
+                totalJ += float(row[13])
+            if row[2] == "Baby Food":
+                totalK += float(row[13])
+            if row[2] == "Cereal":
+                totalL += float(row[13])
+        print("Fruits: %s" % totalA)
+        print()
+        print("Clothes: %s" % totalB)
+        print()
+        print("Meat: %s" % totalC)
+        print()
+        print("Beverages: %s" % totalD)
+        print()
+        print("Office Supplies: %s" % totalE)
+        print()
+        print("Cosmetics: %s" % totalF)
+        print()
+        print("Snacks: %s" % totalG)
+        print()
+        print("Personal Care: %s" % totalH)
+        print()
+        print("Household: %s" % totalI)
+        print()
+        print("Vegetables: %s" % totalJ)
+        print()
+        print("Baby Food: %s" % totalK)
+        print()
+        print("Cereal: %s" % totalL)
         print(item_list)
 
